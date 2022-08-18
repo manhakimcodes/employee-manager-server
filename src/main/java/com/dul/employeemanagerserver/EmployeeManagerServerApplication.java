c2c1c3aadc7166417794dfbaa11368eebfe46de2
manhakimcodes <amusa901@gmail.com>
2022-08-17 22:00:26 -0400

    Initial commit

---
+++ src/main/java/com/dul/employeemanagerserver/EmployeeManagerServerApplication.java
@@ -0,0 +1,12 @@
+package com.dul.employeemanagerserver;
+
+
+import org.springframework.boot.SpringApplication;
+
+public class EmployeeManagerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagerServerApplication.class, args);
+	}
+
+}
